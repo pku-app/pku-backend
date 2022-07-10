6e63b1f2b7a55ad4e128b7c297160b7d0f41c8ec
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-07-10 13:26:23 -0300

    add sing-up and sign-up add JWT and passport for accessToken generation

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,13 +1,31 @@
-import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, ValidationPipe, Request } from "@nestjs/common";
+
 import { UsersDTO } from "src/users/dto/users.dto";
+import { UsersService } from "src/users/users.service";
 import { AuthService } from "../auth.service";
+import { LocalAuthGuard } from "../guards/local-auth.guard";
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   @Post('signup')
-  async signUp(@Body(ValidationPipe) usersDTO: UsersDTO): Promise<void> {
-    return await this.authService.signUp(usersDTO);
+  async signUp(
+    @Body(ValidationPipe) usersDTO: UsersDTO
+  ): Promise<any> {
+    const authUser = await this.authService.signUp(usersDTO);
+    const token = await this.authService.signIn(authUser);
+    const user = this.usersService.extractUserData(authUser);
+
+    return { user, token };
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  async signIn(@Request() req) {
+    return this.authService.signIn(req.user);
   }
 }
