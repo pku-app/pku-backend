b28c32913b78251abd7d4eb83b5faec1c8f17d99
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-07-10 13:28:37 -0300

    add user's GET

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Users } from './interfaces/users.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor() {}
+
+  /**
+   * Extracts relevant user's data from MongoDB Document
+   * @param user - user's object
+   * @returns relevant user's data.
+   */
+  extractUserData(user: Users) {
+    return {
+      id: user['_id'],
+      email: user['email'],
+      name: user['name'],
+      type: user['type'],
+      health_info: user['health_info']
+    };
+  }
+}
