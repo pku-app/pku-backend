09feb9fbfa3a291ad2a1081f8ae2ae1102bd4029
Daniel M Brasil <danielmbrasil@protonmail.com>
2022-08-07 12:03:21 -0300

    create patch method, extracting data to be patched update get method

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, UseGuards, Request } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Patch,
+  Body,
+  ValidationPipe
+} from "@nestjs/common";
+
 import { JwtAuthGuard } from "src/common/guards/jwt-auth.guard";
+import { PatchUsersDTO } from "../dto/patch_users.dto";
 import { UsersService } from "../users.service";
 
 @Controller('users')
@@ -8,9 +18,26 @@ export class UsersController {
     private usersService: UsersService
   ) {}
 
+  /**
+   * GET /users/user - get user's data
+   * @param req - user's payload extracted from Bearer token, which contains user's id(sub)
+   * @returns JSON with user's data
+   */
   @UseGuards(JwtAuthGuard)
   @Get('user')
-  getUser(@Request() req) {
-    return this.usersService.extractUserData(req.user['user']);
+  async getUser(@Request() req) {
+    return await this.usersService.getUser(req.user['user']['sub']);
+  }
+
+  /**
+   * PATCH /users/user - patch user's information such as name and health_info
+   * @param req - user's payload, which contains user's id (sub)
+   * @param newUserData - user's new data to be patched 
+   * @returns JSON containing patched user's data
+   */
+  @UseGuards(JwtAuthGuard)
+  @Patch('user')
+  patchUser(@Request() req, @Body(ValidationPipe) newUserData: PatchUsersDTO) {
+    return this.usersService.patch(req.user['user']['sub'], newUserData);
   }
 }
